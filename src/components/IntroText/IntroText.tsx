a55a49ada227013d0732b229d4707c8541b55295
Piotr Morawski <piotrmorawski90@gmail.com>
2020-03-19 21:19:51 +0100

    refactor: IntroText

--- src/components/IntroText/IntroText.tsx
+++ src/components/IntroText/IntroText.tsx
@@ -4,21 +4,21 @@ import './IntroText.scss'
 const IntroText = (): ReactElement => {
 	return (
 		<section className='IntroText IntroText--centered appFrame--paddingBottom appFrame--paddingTop'>
-			<p>Creating reusable components in React with TypeScript is my jam.</p>
-			<br />
 			<p>
-				Everything can be tested is my motto. Unit tests with Chai/Enzyme and
-				Karma. Occasionally some work in Cypress tests.
+				Creating reusable components in React with TypeScript is my jam. For
+				styles I prefer JSS but sure can get my hands dirty with old-school
+				Sass.
 			</p>
 			<br />
-			{/* <p>
-        I have experience as a member of an agile team working on a large-scale SAAS project in the field of 
-        engineering simulation.
-      </p>
-      <br /> */}
+			<p>
+				Everything can be tested is my motto. Unit tests with Jest. E2E tests?
+				You bet, Cucumber and Puppeteer is my go to for them.
+			</p>
+			<br />
 			<p>
 				My programming role model is Dan Abramov. I follow his blog and Twitter
-				extensively.
+				extensively. Also Wes Bos, I did nearly all of his courses, and listen
+				to Syntax on daily basis.
 			</p>
 		</section>
 	)
