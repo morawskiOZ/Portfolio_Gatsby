2beed9575b6f9d40b279cba597a3ec92588390d7
Piotr Morawski <piotrmorawski90@gmail.com>
2020-03-21 14:05:10 +0100

    refactor: sticky menu behaviour

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,12 +1,48 @@
-import React, { ReactElement, useEffect, useState } from 'react'
+import React, { ReactElement, useEffect, useState, useRef } from 'react'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 import './Menu.scss'
+import { useScrollPosition } from 'src/hooks/useScrollPosition'
+
+const isBrowser = typeof window !== `undefined`
 
 const Menu = (): ReactElement => {
 	const [activeButton, setActiveButton] = useState<number>(null)
 	const [open, setOpen] = useState<boolean>(false)
 	const [isStartPosition, setIsStartPosition] = useState<boolean>(false)
+	const [isFirstTransition, setIsFirstTransition] = useState(false)
+	const [isSticky, setIsSticky] = useState(false)
+	// folded as in hidded during scroll down
+	const [isFolded, setIsFolded] = useState(false)
 
+	const menu = useRef(null)
+
+	useScrollPosition(
+		({ prevPos, currPos }) => {
+			setIsFirstTransition(false)
+			if ((prevPos.y > 50 && currPos.y > 50) || currPos.y > 100) {
+				setIsSticky(true)
+				setIsFirstTransition(true)
+			} else {
+				setIsSticky(false)
+			}
+			// detect first transition to avoid css animation
+			if (prevPos.y === 0) {
+				setIsFirstTransition(true)
+			}
+			// when scrolling up
+			if (currPos.y > prevPos.y) {
+				setIsFolded(true)
+			}
+			// when scrolling dow
+			if (currPos.y < prevPos.y) {
+				setIsFolded(false)
+			}
+		},
+		[isStartPosition],
+		menu,
+		true,
+		300
+	)
 	const buttonArray: (string | number)[] = [
 		1,
 		'Intro',
@@ -22,6 +58,9 @@ const Menu = (): ReactElement => {
 	const position = { x: 0, y: 0 }
 
 	useEffect(() => {
+		if (isBrowser) {
+			menu.current = document.getElementById('mainMenu')
+		}
 		if (position.y === 0) {
 			setIsStartPosition(true)
 			setActiveButton(null)
@@ -30,56 +69,72 @@ const Menu = (): ReactElement => {
 		}
 	}, [position])
 
+	const burgerMenuEventHandler = (): void => {
+		setOpen(!open)
+		setIsFirstTransition(false)
+	}
+
 	return (
-		<div className='menu-container'>
-			{(open || isStartPosition) && (
-				<nav className='menu'>
-					{buttonArray.map((element, index) => {
-						if (element === 1) {
-							return (
-								// eslint-disable-next-line react/no-array-index-key
-								<div className='menu-item--divider' key={`divider${index}`} />
-							)
-						}
-						return (
-							<button
-								className={`menu-item ${activeButton === index &&
-									'menu-item--active'}`}
-								onClick={(): void => setActiveButton(index)}
-								key={element}
-								type='button'
-							>
-								<AnchorLink
-									href={`#${element}`}
-									offset='100'
-									className='menu-link'
-								>
-									{element}
-								</AnchorLink>
-							</button>
-						)
-					})}
-				</nav>
-			)}
+		<div className={`${isSticky ? 'sticky' : ''}`}>
 			<div
-				className={`menu-burgerMenu ${open ? 'menu-burgerMenu--open' : ''}`}
-				onClick={(): void => setOpen(!open)}
-				onKeyDown={(): void => setOpen(!open)}
-				role='button'
-				tabIndex={0}
+				className={`menu-container ${
+					isSticky && isFolded ? 'menu-container--folded' : ''
+				} ${isFirstTransition ? 'menu-container--isFirstTransition' : ''}`}
 			>
+				{(open || isStartPosition) && (
+					<nav
+						className={`menu ${open ? 'menu--open' : ''} ${
+							isFirstTransition ? 'menu--isFirstTransition' : ''
+						}`}
+					>
+						{buttonArray.map((element, index) => {
+							if (element === 1) {
+								return (
+									// eslint-disable-next-line react/no-array-index-key
+									<div className='menu-item--divider' key={`divider${index}`} />
+								)
+							}
+							return (
+								<button
+									className={`menu-item ${activeButton === index &&
+										'menu-item--active'}`}
+									onClick={(): void => setActiveButton(index)}
+									key={element}
+									type='button'
+								>
+									<AnchorLink
+										href={`#${element}`}
+										offset='100'
+										className='menu-link'
+									>
+										{element}
+									</AnchorLink>
+								</button>
+							)
+						})}
+					</nav>
+				)}
 				<div
-					className={`menu-bar1 ${open ? 'menu-bar1--open' : ''}`}
-					key='b1'
-				/>
-				<div
-					className={`menu-bar2 ${open ? 'menu-bar2--open' : ''}`}
-					key='b2'
-				/>
-				<div
-					className={`menu-bar3 ${open ? 'menu-bar3--open' : ''}`}
-					key='b3'
-				/>
+					className={`menu-burgerMenu ${open ? 'menu-burgerMenu--open' : ''}`}
+					onClick={burgerMenuEventHandler}
+					onKeyDown={burgerMenuEventHandler}
+					role='button'
+					tabIndex={0}
+					id='mainMenu'
+				>
+					<div
+						className={`menu-bar1 ${open ? 'menu-bar1--open' : ''}`}
+						key='b1'
+					/>
+					<div
+						className={`menu-bar2 ${open ? 'menu-bar2--open' : ''}`}
+						key='b2'
+					/>
+					<div
+						className={`menu-bar3 ${open ? 'menu-bar3--open' : ''}`}
+						key='b3'
+					/>
+				</div>
 			</div>
 		</div>
 	)
---
+++ src/hooks/useScrollPosition.ts
@@ -0,0 +1,56 @@
+import { useRef, useLayoutEffect } from 'react'
+
+const isBrowser = typeof window !== `undefined`
+
+const getScrollPosition = ({
+	element,
+	useWindow,
+}: {
+	element?: any
+	useWindow: boolean
+}): any => {
+	if (!isBrowser) return { x: 0, y: 0 }
+
+	const target = element ? element.current : document.body
+	if (!target) return { x: 0, y: 0 }
+	const position = target.getBoundingClientRect()
+
+	return useWindow
+		? { x: window.scrollX, y: window.scrollY }
+		: { x: position.left, y: position.top }
+}
+
+export const useScrollPosition = (
+	effect,
+	deps,
+	element,
+	useWindow,
+	wait
+): any => {
+	const position = useRef(getScrollPosition({ useWindow }))
+
+	let throttleTimeout = null
+
+	const callBack = () => {
+		const currPos = getScrollPosition({ element, useWindow })
+		effect({ prevPos: position.current, currPos })
+		position.current = currPos
+		throttleTimeout = null
+	}
+
+	useLayoutEffect(() => {
+		const handleScroll = () => {
+			if (wait) {
+				if (throttleTimeout === null) {
+					throttleTimeout = setTimeout(callBack, wait)
+				}
+			} else {
+				callBack()
+			}
+		}
+
+		window.addEventListener('scroll', handleScroll)
+
+		return () => window.removeEventListener('scroll', handleScroll)
+	}, deps)
+}
