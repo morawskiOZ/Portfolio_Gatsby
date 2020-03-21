fd65c5f731fc46701b8d1c1136d7dccfaf47d591
Piotr Morawski <piotrmorawski90@gmail.com>
2020-03-21 18:08:27 +0100

    test: chanage test case desc

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import renderer from 'react-test-renderer'
 import App from './App'
 
-describe('Header', () => {
+describe('App', () => {
 	it('renders correctly', () => {
 		const tree = renderer.create(<App />).toJSON()
 		expect(tree).toMatchSnapshot()
