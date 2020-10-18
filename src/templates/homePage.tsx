d5d61efc4942ae4db620994b1ead52cec20a304b
morawskiOz <piotrmorawski90@gmail.com>
2020-10-18 17:17:02 +0200

    chore: change font loading

--- src/templates/homePage.tsx
+++ src/templates/homePage.tsx
@@ -1,8 +1,11 @@
 import React, { ReactElement } from 'react'
 import App from 'src/components/App/App'
+import Layout from 'src/components/layout'
 
 const HomePage = ({ pageContext }): ReactElement => (
-	<App images={pageContext.images} />
+	<Layout>
+		<App images={pageContext.images} />
+	</Layout>
 )
 
 export default HomePage
